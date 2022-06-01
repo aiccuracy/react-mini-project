45c653aab842e1ea076bbe91a55c0259841afa44
aiccuracy <soojin.emily@gmail.com>
2022-06-01 11:48:21 +0900

    feat: add birthday reminder

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        name: "Soojin",
+        age: 23,
+        image: "/profile-sooin.JPG",
+    },
+    {
+        id: 2,
+        name: "Haasalmon",
+        age: 23,
+        image: "/profile-sooin.JPG",
+    },
+    {
+        id: 3,
+        name: "Kathymoon",
+        age: 22,
+        image: "/profile-sooin.JPG",
+    },
+];
